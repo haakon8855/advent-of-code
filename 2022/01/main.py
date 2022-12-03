dee7cfb78ead968136b3a2c7d76a0800e0f75512
Håkon Anders <haakon8855@users.noreply.github.com>
2022-12-03 13:42:53 +0100

    Refactor solution for 1st to conform with template

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -1,38 +1,38 @@
 """haakon8855"""
 
+from aoc_utils.utilities import Utilities
 
-def most_calories(path: str) -> int:
+
+def most_calories(data: str) -> int:
     """
     Returns the number of calories the elf with the most calories carries.
     """
-    with open(path, 'r', encoding='utf-8') as file:
-        raw = file.read()
-    raw_list = raw.split('\n\n')
+    raw_list = data.split('\n\n')
     highest = 0
     for elf in raw_list:
         elf_calories = sum(list(map(int, elf.split('\n'))))
         highest = max(highest, elf_calories)
-
     return highest
 
 
-def top_n_most_calories(path: str, num: int = 3) -> int:
+def top_n_most_calories(data: str, num: int = 3) -> int:
     """
     Returns the number of calories the elf with the most calories carries.
     """
-    with open(path, 'r', encoding='utf-8') as file:
-        raw = file.read()
-    raw_list = raw.split('\n\n')
+    raw_list = data.split('\n\n')
     highest = [0] * num
     for elf in raw_list:
         elf_calories = sum(list(map(int, elf.split('\n'))))
         lowest_highest = min(highest)
         index = highest.index(lowest_highest)
         highest[index] = max(lowest_highest, elf_calories)
-
     return sum(highest)
 
 
 if __name__ == "__main__":
-    print(top_n_most_calories("2022/01/test_data.txt"))
-    print(top_n_most_calories("2022/01/data.txt"))
+    TEST_DATA = Utilities.read_data("2022/01/data/test_data.txt")
+    DATA = Utilities.read_data("2022/01/data/data.txt")
+
+    Utilities.print_outputs(most_calories(TEST_DATA), most_calories(DATA),
+                            top_n_most_calories(TEST_DATA),
+                            top_n_most_calories(DATA))
--- 2022/01/test_main.py
+++ 2022/01/test_main.py
@@ -2,18 +2,20 @@
 
 import main
 
+from aoc_utils.utilities import Utilities
+
 
 def test_top_elf_calories():
     """
     Function should return the total amount of calories carried by the elf
     carrying the most calories.
     """
-    test_data_path = "2022/01/test_data.txt"
-    val = main.most_calories(test_data_path)
+    test_data = Utilities.read_data("2022/01/data/test_data.txt")
+    val = main.most_calories(test_data)
     assert val == 24000
 
-    test_data_path = "2022/01/test_data_2.txt"
-    val = main.most_calories(test_data_path)
+    test_data_2 = Utilities.read_data("2022/01/data/test_data_2.txt")
+    val = main.most_calories(test_data_2)
     assert val == 17030
 
 
@@ -22,10 +24,10 @@ def test_top_three_elves_calories():
     Function should return the total amount of calories carried by the three
     elves carrying the most calories.
     """
-    test_data_path = "2022/01/test_data.txt"
-    val = main.top_n_most_calories(test_data_path)
+    test_data = Utilities.read_data("2022/01/data/test_data.txt")
+    val = main.top_n_most_calories(test_data)
     assert val == 45000
 
-    test_data_path = "2022/01/test_data_2.txt"
-    val = main.top_n_most_calories(test_data_path)
+    test_data_2 = Utilities.read_data("2022/01/data/test_data_2.txt")
+    val = main.top_n_most_calories(test_data_2)
     assert val == 35030
