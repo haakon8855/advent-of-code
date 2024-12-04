0fff064ddd9f8e4dc27b58a84196df93698cfb96
Håkon Anders Strømsodd <haakon8855@gmail.com>
2024-12-04 15:18:25 +0100

    4. dec 2024

---
+++ 2024/04/FirstSolution.cs
@@ -0,0 +1,158 @@
+namespace Three;
+
+public class FirstSolution
+{
+    public static int PartOne(string[] lines)
+    {
+        var target = "XMAS";
+        var targetArray = target.ToCharArray();
+        Array.Reverse(targetArray);
+        var reverseTarget = new string(targetArray);
+
+        var count = 0;
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            for (var j = 0; j < line.Length; j++)
+            {
+                if (line[j] == 'X')
+                {
+                    // if there is space for XMAS to the right
+                    if (j <= line.Length - 4)
+                    {
+                        // check right
+                        if (line.Substring(j, 4) == target)
+                            count++;
+
+                        // if there is space up
+                        if (i >= 3)
+                        {
+                            // check up-right
+                            count++;
+                            for (var k = 0; k < 4; k++)
+                            {
+                                if (lines[i - k][j + k] != target[k])
+                                {
+                                    count--;
+                                    break;
+                                }
+                            }
+                        }
+
+                        // if there is space down
+                        if (i <= lines.Length - 4)
+                        {
+                            // check down-right
+                            count++;
+                            for (var k = 0; k < 4; k++)
+                            {
+                                if (lines[i + k][j + k] != target[k])
+                                {
+                                    count--;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    // if there is space left
+                    if (j >= 3)
+                    {
+                        // check left
+                        if (line.Substring(j - 3, 4) == reverseTarget)
+                            count++;
+
+                        // if there is space up
+                        if (i >= 3)
+                        {
+                            // check up-left
+                            count++;
+                            for (var k = 0; k < 4; k++)
+                            {
+                                if (lines[i - k][j - k] != target[k])
+                                {
+                                    count--;
+                                    break;
+                                }
+                            }
+                        }
+
+                        // if there is space down
+                        if (i <= lines.Length - 4)
+                        {
+                            // check down-left
+                            count++;
+                            for (var k = 0; k < 4; k++)
+                            {
+                                if (lines[i + k][j - k] != target[k])
+                                {
+                                    count--;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    // if there is space up
+                    if (i >= 3)
+                    {
+                        // check up
+                        count++;
+                        for (var k = 0; k < 4; k++)
+                        {
+                            if (lines[i - k][j] != target[k])
+                            {
+                                count--;
+                                break;
+                            }
+                        }
+                    }
+
+                    // if there is space down
+                    if (i <= lines.Length - 4)
+                    {
+                        // check down
+                        count++;
+                        for (var k = 0; k < 4; k++)
+                        {
+                            if (lines[i + k][j] != target[k])
+                            {
+                                count--;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static int PartTwo(string[] lines)
+    {
+        var count = 0;
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            for (var j = 0; j < line.Length; j++)
+            {
+                if (line[j] == 'A')
+                {
+                    if (i > 0 && i < lines.Length - 1 && j > 0 && j < line.Length - 1)
+                    {
+                        if ((lines[i - 1][j - 1] == 'M' && lines[i + 1][j + 1] == 'S' ||
+                             lines[i - 1][j - 1] == 'S' && lines[i + 1][j + 1] == 'M') &&
+                            (lines[i + 1][j - 1] == 'M' && lines[i - 1][j + 1] == 'S' ||
+                             lines[i + 1][j - 1] == 'S' && lines[i - 1][j + 1] == 'M'))
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
---
+++ 2024/04/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+using Three;
+using Utils;
+
+var test = false;
+Chronograph chronograph = new Chronograph();
+var path = test ? "test.txt" : "input.txt";
+var lines = File.ReadAllLines(path);
+
+// ======================= 
+//        Part 1
+// ======================= 
+Console.WriteLine("Part 1: ");
+chronograph.Toggle();
+// ======================= 
+
+
+var width = lines.Length;
+var patterns = new[]
+{
+    $"(?=(xmas|samx))", // Left-right
+    $"(?=(s.{{{width - 1}}}a.{{{width - 1}}}m.{{{width - 1}}}x|x.{{{width - 1}}}m.{{{width - 1}}}a.{{{width - 1}}}s))", // bottom-left > top-right diagonals
+    $"(?=(x.{{{width}}}m.{{{width}}}a.{{{width}}}s|s.{{{width}}}a.{{{width}}}m.{{{width}}}x))", // Up-down
+    $"(?=(x.{{{width + 1}}}m.{{{width + 1}}}a.{{{width + 1}}}s|s.{{{width + 1}}}a.{{{width + 1}}}m.{{{width + 1}}}x))" // top-left > bottom-right diagonals
+};
+
+var allLines = string.Join("\n", lines).ToLower();
+var count = patterns.Sum(p => Regex.Matches(allLines, p, RegexOptions.Singleline).Count);
+Console.WriteLine($"Regex solution count: {count}");
+chronograph.Toggle();
+
+
+chronograph.Toggle();
+count = FirstSolution.PartOne(lines);
+Console.WriteLine($"First solution count: {count}");
+chronograph.Toggle();
+
+// =======================
+//        Part 2
+// ======================= 
+Console.WriteLine("\nPart 2: ");
+chronograph.Toggle();
+// ======================= 
+
+var pattern =
+    $"(?=(?<=m.m.{{{width - 1}}})a(?=.{{{width - 1}}}s.s)|(?<=s.s.{{{width - 1}}})a(?=.{{{width - 1}}}m.m)|(?<=s.m.{{{width - 1}}})a(?=.{{{width - 1}}}s.m)|(?<=m.s.{{{width - 1}}})a(?=.{{{width - 1}}}m.s))";
+count = Regex.Matches(allLines, pattern, RegexOptions.Singleline).Count;
+Console.WriteLine($"Regex solution count: {count}");
+chronograph.Toggle();
+
+
+chronograph.Toggle();
+count = FirstSolution.PartTwo(lines);
+Console.WriteLine($"First solution count: {count}");
+
+chronograph.Toggle();
